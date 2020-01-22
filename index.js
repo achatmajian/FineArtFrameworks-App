bb8b87babd6136ff94f8bd1592aafb74fbba93f3
Arek Chatmajian <50626603+achatmajian@users.noreply.github.com>
2020-01-22 12:35:35 -0500

    Mat float flush logic (#17)
    * added finishing options
    
    * applied jquery enable/disable logic to mat section
    
    * added JS toggle logic for mat, float, and flush checkboxes

--- index.js
+++ index.js
@@ -7,16 +7,75 @@
 //     });
 // });
 
-// Mat checkbox to activate radio options
-
+// Mat checkbox activation
 $(document).ready(function() {
   $("#mat-checkbox").click(function(){
     if ($(this).is(':checked')) 
     {
       $(".mat-answer").prop("disabled", false);
+      $(".mat-colors").prop("disabled", false);
+      $("#float-checkbox").prop("disabled", true);
+      $(".float-answer").prop("disabled", true);
+      $("#flush-checkbox").prop("disabled", true);
+      $("#spacers-checkbox").prop("disabled", true);
+      $(".spacer-answers").prop("disabled", true);
 
     } else {
           $(".mat-answer").prop("disabled", true);
+          $(".mat-colors").prop("disabled", true);
+          $("#float-checkbox").prop("disabled", false);
+          $(".float-answer").prop("disabled", false);
+          $("#flush-checkbox").prop("disabled", false);
+          $("#spacers-checkbox").prop("disabled", false);
+          $(".spacer-answers").prop("disabled", false);
+    }
+  });
+});
+
+
+// Float checkbox activation
+$(document).ready(function() {
+  $("#float-checkbox").click(function(){
+    if ($(this).is(':checked')) 
+    {
+      $(".float-answer").prop("disabled", false);
+      $("#mat-checkbox").prop("disabled", true);
+      $(".mat-answer").prop("disabled", true);
+      $(".mat-colors").prop("disabled", true);
+      $("#flush-checkbox").prop("disabled", true);
+      $("#drymount-checkbox").prop("disabled", true);
+      $(".drymount-answers").prop("disabled", true);
+
+    } else {
+          $(".float-answer").prop("disabled", true);
+          $("#mat-checkbox").prop("disabled", false);
+          $(".mat-answer").prop("disabled", false);
+          $(".mat-colors").prop("disabled", false);
+          $("#flush-checkbox").prop("disabled", false);
+          $("#drymount-checkbox").prop("disabled", false);
+          $(".drymount-answers").prop("disabled", false);
+    }
+  });
+});
+
+// Flush checkbox activation
+$(document).ready(function() {
+  $("#flush-checkbox").click(function(){
+    if ($(this).is(':checked')) 
+    {
+      $("#float-checkbox").prop("disabled", true);
+      $(".float-answer").prop("disabled", true);
+      $("#mat-checkbox").prop("disabled", true);
+      $(".mat-answer").prop("disabled", true);
+      $(".mat-colors").prop("disabled", true);
+
+    } else {
+        $("#float-checkbox").prop("disabled", false);
+        $(".float-answer").prop("disabled", false);
+        $("#mat-checkbox").prop("disabled", false);
+        $(".mat-answer").prop("disabled", false);
+        $(".mat-colors").prop("disabled", false);
+
     }
   });
 });
