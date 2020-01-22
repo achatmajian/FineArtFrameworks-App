5193b71f31281799d6990e0294f6ec4dda5d3bcc
Arek Chatmajian <50626603+achatmajian@users.noreply.github.com>
2020-01-22 11:26:11 -0500

    Mat jquery (#16)
    * added finishing options
    
    * applied jquery enable/disable logic to mat section

--- index.js
+++ index.js
@@ -1,25 +1,22 @@
 // Append new "Build Frame" form box below the previous one
 
-$(document).ready(function(){
-    $("#add-frame-button").click(function(){
-        // $("#bottom").append("#build-frame"); 
-        $("#build-frame").clone().insertAfter("div.frame-form:last");
-    });
-});
+// $(document).ready(function(){
+//     $("#add-frame-button").click(function(){
+//         // $("#bottom").append("#build-frame"); 
+//         $("#build-frame").clone().insertAfter("div.frame-form:last");
+//     });
+// });
 
 // Mat checkbox to activate radio options
 
-function mat(res) { 
-    if(document.getElementById("mat-checkbox").checked){
-      document.getElementById("4ply").removeAttribute('disabled');
-      document.getElementById("8ply").removeAttribute('disabled');
-      document.getElementById("12ply").removeAttribute('disabled');
-      document.getElementById("fabric").removeAttribute('disabled');
+$(document).ready(function() {
+  $("#mat-checkbox").click(function(){
+    if ($(this).is(':checked')) 
+    {
+      $(".mat-answer").prop("disabled", false);
+
+    } else {
+          $(".mat-answer").prop("disabled", true);
     }
-    else{
-      document.getElementById("4ply").disabled = true;
-      document.getElementById("8ply").disabled = true;
-      document.getElementById("12ply").disabled = true;
-      document.getElementById("fabric").disabled = true;
-    }
-  }
+  });
+});
