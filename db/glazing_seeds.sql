9731e91dbe181892b5c3a0f0dc3163e4641c6964
Arek Chatmajian <arek.chatmajian@gmail.com>
2020-04-23 15:53:04 -0400

    fixed glazing table typo

--- db/glazing_seeds.sql
+++ db/glazing_seeds.sql
@@ -16,7 +16,7 @@ VALUES
 ("glass", "Regular Glass", 8.00),
 ("glass", "Tru Vue Ultra Vue", 21.00),
 ("glass", "Museum Glass", 35.00),
-("glass", "Museum Glass", 35.00),
+("glass", "Art Glass", 35.00),
 ("plexiglass", "Regular Plexiglass", 12.00),
 ("plexiglass", "UV Plexiglass", 19.00),
 --Haven't included Museum Plexiglass, as is it priced per frame instead of per foot. Need to figure out how to apply certain things that require a flat fee.
