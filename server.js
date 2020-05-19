9d943969107da5374c2e4eb48c983587a3114a7a
k-lord <katielord27@gmail.com>
2020-05-19 13:04:09 -0400

    added console.logs to see errors, users, and info from the post request

--- server.js
+++ server.js
@@ -10,6 +10,19 @@ var bodyParser = require("body-parser");
 var passport = require('passport')
 var LocalStrategy = require('passport-local').Strategy;
 
+// Create the server app
+// ------------------------------------------------------------------------
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+// Middleware Configurations
+// ------------------------------------------------------------------------
+app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false }));
+//app.use(express.json());
+//app.use(express.bodyParser());
+app.use(bodyParser.json());
+
 // PassportJS User Local Authentication
 // ------------------------------------------------------------------------
 passport.use(new LocalStrategy(
@@ -19,7 +32,7 @@ passport.use(new LocalStrategy(
     passReqToCallback: true
   },
   (req, username, password, done) => {
-    db.user.findOne({ where: { username: email } }, function (err, user) {
+    db.user.findOne({ where: { username: username } }, function (err, user) {
       console.log("Inside local strategy callback");
       //console.log(user);
 
@@ -48,19 +61,13 @@ passport.serializeUser((user, done) => {
   done(null, user.id);
 });
 
-
-// Create the server app
+// More Middleware Configurations
 // ------------------------------------------------------------------------
-var app = express();
-var PORT = process.env.PORT || 3000;
-
-// Middleware Configuration
-// ------------------------------------------------------------------------
-app.use(express.urlencoded({ extended: false }));
-app.use(bodyParser.urlencoded({ extended: false }));
+//app.use(express.urlencoded({ extended: false }));
+//app.use(bodyParser.urlencoded({ extended: false }));
 //app.use(express.json());
 //app.use(express.bodyParser());
-app.use(bodyParser.json());
+//app.use(bodyParser.json());
 app.use(express.static(__dirname + "/public"));
 app.use(session({
   genid: (req) => {
@@ -68,7 +75,7 @@ app.use(session({
     console.log("request object sessionID from client: " + JSON.stringify(req.sessionID));
     //console.log(req);
     return uuidv4(); // use UUIDs for session IDs
-    
+
   },
   store: new FileStore(),
   secret: "cats",
@@ -84,19 +91,43 @@ app.post("/login", (req, res, next) => {
   console.log("Inside the POST /login callback function");
   passport.authenticate('local', (err, user, info) => {
     console.log("Inside the passport.authenticate() callback");
-    console.log("req.session.passport: " + req.session.passport);
-    console.log("req.user: " + req.user);
-    req.login(user, (err) => {
-      console.log("Inside the req.login() callback");
-      console.log("req.session.passport: " + req.session.passport);
-      console.log("req.user: " + req.user);
-      return res.send("You were authenticated and logged in!");
-    })
-  })(req, res, next);
-  //console.log(req.body);
-  //res.send("You posted to the login page!\n");
-});
+    // this will execute in any case, even if a passport strategy will find an error
+    // log everything to console
+    console.log("errors: \n\n" + err + "\n\n");
+    console.log("user: \n\n" + user) + "\n\n";
+    console.log("info: \n\n" + info);
 
+    if (err) {
+      res.status(401).send(err);
+    } else if (!user) {
+      res.status(401).send(JSON.stringify(info));
+    } else {
+      next();
+    }
+
+    res.status(401).send(info);
+  })(req, res);
+},
+
+  // function to call once successfully authenticated
+  function (req, res) {
+    res.status(200).send('logged in!');
+  });
+
+/*console.log("Inside the passport.authenticate() callback");
+console.log("req.session.passport: " + req.session.passport);
+console.log("req.user: " + req.user);
+req.login(user, (err) => {
+  console.log("Inside the req.login() callback");
+  console.log("req.session.passport: " + req.session.passport);
+  console.log("req.user: " + req.user);
+  return res.send("You were authenticated and logged in!");
+})
+})(req, res);
+//console.log(req.body);
+//res.send("You posted to the login page!\n");
+});
+*/
 // Routes
 // ------------------------------------------------------------------------
 require("./routes/api/client-routes")(app);
